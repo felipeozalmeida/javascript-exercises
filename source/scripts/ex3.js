d91f87fb86c5293bdc432cd6fd454e0a7cba9994
Felipe Almeida <felipeortalmeida@gmail.com>
2019-04-24 19:07:32 -0300

    Moved exercise 3's scripts to a separate JS file

---
+++ source/scripts/ex3.js
@@ -0,0 +1,7 @@
+for (let i = 1; i <= 10; i++) {
+    document.write(`${i} times table<br><br>`);
+    for (let j = 1; j <= 10; j++) {
+        document.write(`${j} * ${i} = ${j * i}<br>`);
+    }
+    document.write("<br>");
+}
