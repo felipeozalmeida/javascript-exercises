602d51fe78207da30e0c5629d3a6b7769a417068
Felipe Almeida <felipeortalmeida@gmail.com>
2019-05-05 22:36:10 -0300

    Linted ex4.js

--- source/scripts/ex4.js
+++ source/scripts/ex4.js
@@ -1,19 +1,19 @@
 let groups = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-];
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+]
 groups.forEach((group, groupIndex) => {
-    group.forEach((value, index, group) => {
-        group[index] = prompt(`Insert number ${index + 1} for group ${groupIndex + 1}`);
-    });
-});
+  group.forEach((value, index, group) => {
+    group[index] = prompt(`Insert number ${index + 1} for group ${groupIndex + 1}`)
+  })
+})
 groups.forEach((group, groupIndex) => {
-    document.write(`Group ${groupIndex + 1}<br><br>`);
-    group.forEach((value, index) => {
-        document.write(`groups[${groupIndex}][${index}] = ${value}<br>`);
-    });
-    document.write("<br>");
-});
+  document.write(`Group ${groupIndex + 1}<br><br>`)
+  group.forEach((value, index) => {
+    document.write(`groups[${groupIndex}][${index}] = ${value}<br>`)
+  })
+  document.write('<br>')
+})
